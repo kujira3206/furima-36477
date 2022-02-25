78c3ba5aa70e95a7088762867dc309389228123b
kujira3206 <rkujiraoka1210@gmail.com>
2022-02-25 12:39:26 +0900

    createアクションのコントローラーを実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,5 +6,15 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    Product.create(product_params)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:product_name, :product_explanation, :category_id, :product_status_id, :shipping_charge_id, :shipping_area_id, :ship_id, :price) 
+  end
   
 end
