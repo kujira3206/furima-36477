44a81a06c9c75ca9cf8ab84f3ad9ef75e10fc502
kujira3206 <rkujiraoka1210@gmail.com>
2022-02-25 12:50:14 +0900

    画像の保存を実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:product_name, :product_explanation, :category_id, :product_status_id, :shipping_charge_id, :shipping_area_id, :ship_id, :price) 
+  params.require(:product).permit(:product_name, :product_explanation, :category_id, :product_status_id, :shipping_charge_id, :shipping_area_id, :ship_id, :price, :image).merge(user_id: current_user.id)  
   end
   
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :product_name, presence: true
+  validates :product_explanation, presence: true
+  validates :category_id, presence: true
+  validates :product_status_id, presence: true
+  validates :shipping_charge_id, presence: true
+  validates :shipping_area_id, presence: true
+  validates :ship_id, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
 end
