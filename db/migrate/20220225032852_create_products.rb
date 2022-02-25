2458becffea1d3fd5f62d32029231dd475725f1b
kujira3206 <rkujiraoka1210@gmail.com>
2022-02-25 12:31:06 +0900

    productモデルの作成

---
+++ db/migrate/20220225032852_create_products.rb
@@ -0,0 +1,18 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+
+      t.string :product_name, null: false
+      t.text :Product_explanation, null: false
+      t.integer :category_id, numericality: { other_than: 1 } 
+      t.integer :product_status_id, numericality: { other_than: 1 } 
+      t.integer :shipping_charge_id, numericality: { other_than: 1 } 
+      t.integer :shipping_area_id, numericality: { other_than: 1 } 
+      t.integer :ship_id, numericality: { other_than: 1 } 
+      t.integer :price, null:false
+      t.references :user, null: false, foreign_kye: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_18_110323) do
+ActiveRecord::Schema.define(version: 2022_02_25_032852) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "product_name", null: false
+    t.text "Product_explanation", null: false
+    t.integer "category_id"
+    t.integer "product_status_id"
+    t.integer "shipping_charge_id"
+    t.integer "shipping_area_id"
+    t.integer "ship_id"
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
