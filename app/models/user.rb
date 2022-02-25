20e05294d1b97bfc307881251bc2d6fa8c402d45
kujira3206 <rkujiraoka1210@gmail.com>
2022-02-25 12:33:16 +0900

    アソシエーションの実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User < ApplicationRecord
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX}
 
-  # has_many :products
+  has_many :products
   # has_many :purchases
 end
