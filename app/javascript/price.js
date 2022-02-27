ca8797685be12db58d197529277bb6fda20f764d
kujira3206 <rkujiraoka1210@gmail.com>
2022-02-27 15:49:06 +0900

    販売利益、販売手数料の表示実装

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,5 +1,13 @@
-function post (){
-  console.log("イベント発火");
-};
+window.addEventListener('load', function(){
 
-window.addEventListener('load', post);
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById('add-tax-price');
+    addTaxDom.innerHTML = Math.floor(inputValue / 10);
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);    
+  })
+
+  
+})
