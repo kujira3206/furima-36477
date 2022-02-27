900bbab1a62d142fd55e460a07aeced8ad1accb5
kujira3206 <rkujiraoka1210@gmail.com>
2022-02-27 16:47:58 +0900

    productモデルの一部記述を削除

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,6 +16,5 @@ class Product < ApplicationRecord
   validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :ship_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-  validates :user, presence: true
   validates :image, presence: true
 end
