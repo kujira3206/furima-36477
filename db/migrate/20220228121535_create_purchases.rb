61513b24acb2eb2db871abb44bce4971b7259de0
kujira3206 <rkujiraoka1210@gmail.com>
2022-02-28 21:42:35 +0900

    マイグレーションの記述

--- db/migrate/20220228121535_create_purchases.rb
+++ db/migrate/20220228121535_create_purchases.rb
@@ -2,6 +2,9 @@ class CreatePurchases < ActiveRecord::Migration[6.0]
   def change
     create_table :purchases do |t|
 
+      t.references :user, null: false, foreign_kye: true
+      t.references :product, null: false, foreign_kye: true
+
       t.timestamps
     end
   end
--- db/migrate/20220228121838_create_shipping_addresses.rb
+++ db/migrate/20220228121838_create_shipping_addresses.rb
@@ -2,6 +2,16 @@ class CreateShippingAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :shipping_addresses do |t|
 
+      t.references :purchase, null: false, foreign_kye: true
+      t.string :zip_code, null: false
+      t.integer :shipping_area_id, numericality: { other_than: 1 } 
+      t.string :minicipalities, null: false
+      t.string :block_number, null: false
+      t.string :building
+      t.string :tel, null: false
+
+
+
       t.timestamps
     end
   end
