8792a5f72907c2b0af92a89aa5631a2024de5e2e
kujira3206 <rkujiraoka1210@gmail.com>
2022-02-28 22:10:34 +0900

    purchaseコントローラーの作成、記述

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,27 @@
+class PurchasesController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @purchase_shipping_address = PurchaseShippingAddress.new
+  end
+
+  def create
+    @purchase_shipping_address = PurchaseShippingAddress.new(purchase_params)
+    if @purchase_shipping_address.valid?
+      @purchase_shipping_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_shipping_address).permit(:zip_code, :shipping_area_id, :minicipalities, :block_number, :building, :tel, :product_id ).merge(user_id: current_user.id )
+  end
+  
+
+end
