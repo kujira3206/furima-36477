edbf46fcb122851a9538eeff87c04ee27c06059b
kujira3206 <rkujiraoka1210@gmail.com>
2022-02-28 22:14:13 +0900

    ルーティングの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'products#index'
-  resources :products
+  resources :products do
+    resources :purchases, only: [:new, :create]
+  end
+
 end
