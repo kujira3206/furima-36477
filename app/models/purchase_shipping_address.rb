c8bb2f50f2a01db39ccd2f117101dacbfca0574e
kujira3206 <rkujiraoka1210@gmail.com>
2022-03-01 16:09:37 +0900

    バリデーションを実装

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,17 +1,16 @@
 class PurchaseShippingAddress
   include ActiveModel::Model
-  attr_accessor :user_id, :product_id, :zip_code, :shipping_area_id, :minicipalities, :block_number, :building, :tel, :purchase_id
+  attr_accessor :user_id, :product_id, :zip_code, :shipping_area, :minicipalities, :block_number, :building, :tel
+  
+  with_options presence: true do
+    validates :zip_code,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :minicipalities 
+    validates :block_number
+    validates :building
+    validates :tel 
+  end
+  validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank" }
 
-
-    # validates :user_id
-    # validates :product_id 
-    # validates :zip_code,format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    # validates :shipping_area_id, presence: true
-    # validates :minicipalities, pressence: true
-    # validates :block_number, presence: true
-    # validates :building
-    # validates :tel, presence: true
-    # validates :purchase_id
 
   def save
     purchase = Purchase.create(product_id: product_id, user_id: user_id)
