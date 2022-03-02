a0b5fb91aed3d2cc47447c7865459f899e47b0e1
kujira3206 <rkujiraoka1210@gmail.com>
2022-03-02 18:01:10 +0900

    FactoryBotを設定

--- spec/factories/purchase_shipping_addresses.rb
+++ spec/factories/purchase_shipping_addresses.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :purchase_shipping_address do
     zip_code {"123-4567"}
-
+    shipping_area {3}
+    minicipalities {"あ"}
+    block_number {"あ"}
+    building {"あ"}
+    tel {12345678901}
   end
 end
