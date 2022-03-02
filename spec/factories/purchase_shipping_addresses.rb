51cbfbacf9f32bae952ed0a5854f96e401190e25
kujira3206 <rkujiraoka1210@gmail.com>
2022-03-02 19:18:23 +0900

    単体テスト準備

--- spec/factories/purchase_shipping_addresses.rb
+++ spec/factories/purchase_shipping_addresses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :purchase_shipping_address do
     zip_code {"123-4567"}
-    shipping_area {3}
+    shipping_area_id {3}
     minicipalities {"あ"}
     block_number {"あ"}
     building {"あ"}
--- spec/models/purchase_shipping_address_spec.rb
+++ spec/models/purchase_shipping_address_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe PurchaseShippingAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '購入情報の保存' do
+    before do
+      product = FactoryBot.create(:product)
+      user = FactoryBot.create(:user)
+      @purchase_shipping_address = FactoryBot.build(:purchase_shipping_address, product_id: product.ship_id)
+     
+    end
+
+    context '内容に問題がない場合' do
+      it '全ての値が正しく入力されていれば保存できること' do
+      end
+      it 'buildingはからでも保存できること' do
+      end
+    end
+
+    context '内容に問題がある場合' do
+      it 'zip_codeが空だと保存できない' do
+      end
+      it 'zip_codeが半角のハイフンを含んでないと保存できないこと' do
+      end
+      it 'zip_codeが半角数字でないと保存できないこと' do
+      end
+      it 'shipping_areaを選択していないと保存できないこと' do
+      end
+      it 'minicipalitiesが空だと保存できないこと' do
+      end
+      it 'block_numberが空だと保存できないこと' do
+      end
+      it 'telが空だと保存できないこと' do
+      end
+      it 'telは10桁以内は保存できない' do
+      end
+      it 'telは11桁以上は保存できない' do
+      end
+      it 'telは半角数字以外は保存できない' do
+      end
+      it 'productが紐づいてないと保存できない' do
+      end
+      it 'userが紐づいてないと保存できない' do
+      end
+
+    end
+  end
 end
